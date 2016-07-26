3b8a9aefa7f0b2a37244ad2c6ac1ae66e188cca1
Luis Carlos González Hernández <lcarlosglez@gmail.com>
2016-07-26 13:38:06 -0500

    Add stage-0 for support async / await syntax

--- config/babel.dev.js
+++ config/babel.dev.js
@@ -13,11 +13,11 @@ module.exports = {
     'babel-preset-es2015',
     'babel-preset-es2016',
     'babel-preset-react'
+    'babel-preset-stage-0'
   ].map(require.resolve),
   plugins: [
     'babel-plugin-syntax-trailing-function-commas',
     'babel-plugin-transform-class-properties',
     'babel-plugin-transform-object-rest-spread',
-    'babel-plugin-syntax-async-functions'
   ].map(require.resolve)
 };
